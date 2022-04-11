09ada989a65e3fd5c18e66066ecca46871963b4a
Alejo9601 <alejandro96jm@gmail.com>
2022-04-11 08:11:14 -0300

    Added icons for paperwork item, several styles changed

--- src/components/PaperworkGrid.js
+++ src/components/PaperworkGrid.js
@@ -5,16 +5,19 @@ const PaperworkGrid = () => {
   return (
     <div className="grid-container" id="paperwork">
       <PaperworkItem
-        title="Tramite 1"
+        title="Consultas"
         description="Descripcion del servicio que no vale un pco pero bueno para algo "
+        image={require("../assets/icon/apoyo.png")}
       />
       <PaperworkItem
-        title="Tramite 1"
+        title="Turnos"
         description="Descripcion del servicio que no vale un pco pero bueno para algo "
+        image={require("../assets/icon/disponibilidad.png")}
       />
       <PaperworkItem
-        title="Tramite 1"
+        title="Impuestos"
         description="Descripcion del servicio que no vale un pco pero bueno para algo "
+        image={require("../assets/icon/taxes.png")}
       />
     </div>
   );
--- src/components/PaperworkItem.js
+++ src/components/PaperworkItem.js
@@ -1,12 +1,14 @@
-const PaperworkItem = ({ title, description }) => {
+const PaperworkItem = ({ title, description, image}) => {
   return (
     <div className="paperwork-item flex-container">
       <picture className="paperwork-icon">
-        <source />
+        <source srcSet={image}/>
         <img alt="paperwork" />
       </picture>
-      <h2 className="paperwork-title">{title}</h2>
-      <p className="peperwork-description">{description}</p>
+      <div className="description-container"> 
+        <h2 className="paperwork-title">{title}</h2>
+        <p className="paperwork-description">{description}</p>
+      </div>
     </div>
   );
 };
