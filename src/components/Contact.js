dc57dc6503420b100ca5bd8fd084e81eca68bcfb
Alejo9601 <alejandro96jm@gmail.com>
2022-04-12 08:11:08 -0300

    Refactored Contact and Contact Means

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,5 @@
 import ContactForm from "./ContactForm";
 import ContactMeans from "./ContactMeans";
-import whatsapp from "../assets/icon/whatsapp.png";
-import email from "../assets/icon/email.png";
 
 const Contact = () => {
   return (
@@ -10,7 +8,7 @@ const Contact = () => {
         <h3>Dejanos tu mensaje</h3>
       </div>
       <ContactForm />
-      <ContactMeans img1={whatsapp} img2={email} />
+      <ContactMeans />
     </>
   );
 };
--- src/components/ContactMeans.js
+++ src/components/ContactMeans.js
@@ -1,13 +1,16 @@
+import whatsapp from "../assets/icon/whatsapp.png";
+import email from "../assets/icon/email.png";
+
 const ContactMeans = ({ img1, img2 }) => {
   return (
     <div className="flex-container" id="contact-means">
       <h3>O Contactanos directamente a estos medios</h3>
       <picture className="img-container" id="whatsapp">
-        <source srcSet={img1} />
+        <source srcSet={whatsapp} />
         <img alt="whatsapp" />
       </picture>
       <picture className="img-container" id="email">
-        <source srcSet={img2} />
+        <source srcSet={email} />
         <img alt="email" />
       </picture>
     </div>
