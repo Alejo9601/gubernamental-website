20fbac69b29131d3eb0d7c7260565d5912991b12
Alejo9601 <alejandro96jm@gmail.com>
2022-04-19 11:41:31 -0300

    Added buttons on newslider, not working yetgit add .

--- src/components/NewsSlider.js
+++ src/components/NewsSlider.js
@@ -1,21 +1,57 @@
+import { useRef } from "react";
 import "../styles/newsSlider.css";
 import NewsCard from "./NewsCard";
 
 const NewsSlider = () => {
+  const cardsCarousel = useRef();
+
+  const handleNext = () => {
+    // console.log("next");
+    // console.log(cardsCarousel.current);
+    cardsCarousel.current.style.transform = "translate(300px)";
+  };
+
+  const handlePrev = () => {
+    // console.log("prev");
+    // console.log(cardsCarousel.current);
+  };
+
   return (
     <div className="flex-container" id="news-slider">
-      <NewsCard
-        newstitle="Noticia 1"
-        description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
-      />
-      <NewsCard
-        newstitle="Noticia 1"
-        description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
-      />
-      <NewsCard
-        newstitle="Noticia 1"
-        description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
-      />
+      <div className="prev-card" onClick={handlePrev}>
+        {`<`}
+      </div>
+      <div ref={cardsCarousel} className="flex-container" id="cards-carousel">
+        <NewsCard
+          className="first-news"
+          newstitle="Noticia 1"
+          description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
+        />
+        <NewsCard
+          newstitle="Noticia 1"
+          description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
+        />
+        <NewsCard
+          newstitle="Noticia 1"
+          description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
+        />
+        <NewsCard
+          newstitle="Noticia 1"
+          description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
+        />
+        <NewsCard
+          newstitle="Noticia 1"
+          description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
+        />
+        <NewsCard
+          className="last-news"
+          newstitle="Noticia 1"
+          description="Cinco siglos más tarde Lorem Ipsum experimentó una oleada de popularidad con el lanzamiento de hojas de transferencia seca de Letraset."
+        />
+      </div>
+      <div className="next-card" onClick={handleNext}>
+        {`>`}
+      </div>
     </div>
   );
 };
