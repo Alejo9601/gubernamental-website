7eb9a17cb8d7dfaee427411e750fb7b3ef109f9b
Alejo9601 <alejandro96jm@gmail.com>
2022-04-26 17:03:08 -0300

    Deleted contact section title, other styles fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,6 @@ import ContactMeans from "./ContactMeans";
 const Contact = () => {
   return (
     <section className="section-content" id="contact">
-      <div id="contact-title">
-        <h3>Leave us your message</h3>
-      </div>
       <ContactForm />
       <ContactMeans />
     </section>
