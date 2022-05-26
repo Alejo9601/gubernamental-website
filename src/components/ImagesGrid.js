b8aaf6f39e91b3c308e642238fb6eca2d048fcd3
Alejo9601 <alejandro96jm@gmail.com>
2022-05-26 10:31:41 -0300

    Little refactor

--- src/components/ImagesGrid.js
+++ src/components/ImagesGrid.js
@@ -1,45 +1,27 @@
 import "../styles/gallery.css";
 
 const ImagesGrid = () => {
-  const images = ["cerro.jpg", "el_chalten.jpg", "glaciares.jpg", "inicio.jpg"];
+  const images = [
+    "cerro.jpg",
+    "el_chalten.jpg",
+    "glaciares.jpg",
+    "cerro.jpg",
+    "el_chalten.jpg",
+    "glaciares.jpg",
+    "cerro.jpg",
+    "el_chalten.jpg",
+    "glaciares.jpg",
+  ];
   return (
     <div className="grid-container">
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[0]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[1]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[2]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[3]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[0]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[1]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[2]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[3]}`)} />
-        <img alt="gallery" />
-      </picture>
-      <picture className="img-container gallery">
-        <source srcSet={require(`../assets/img/${images[0]}`)} />
-        <img alt="gallery" />
-      </picture>
+      {images.map((image) => {
+        return (
+          <picture className="img-container gallery">
+            <source srcSet={require(`../assets/img/${image}`)} />
+            <img alt="gallery" />
+          </picture>
+        );
+      })}
     </div>
   );
 };
