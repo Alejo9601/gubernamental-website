8c614b3583d8c330878b05fec4509bdcbb1df7c2
Alejo9601 <alejandro96jm@gmail.com>
2022-05-26 14:28:24 -0300

    Modified submenu text values

---
+++ src/components/SubmenuGovernment.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const SubmenuGovernment = () => {
+  return (
+    <ol className="submenu">
+      <Link to="/government">Politician</Link>
+      <Link to="/government">Politician</Link>
+      <Link to="/government">Politician</Link>
+      <Link to="/government">Politician</Link>
+    </ol>
+  );
+};
+
+export default SubmenuGovernment;
