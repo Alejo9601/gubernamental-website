442cd0d8690372465bdf2f5726f5526c0e25ce8f
Alejo9601 <alejandro96jm@gmail.com>
2022-06-09 16:36:56 -0300

    Feat, added title and description for contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,6 +5,15 @@ import "../styles/contactPhones.css";
 const Contacts = () => {
   return (
     <section className="section-content" id="contacts">
+      <div className="flex-container" id="title-description">
+        <h1>Useful contacts</h1>
+        <p className="description">
+          Adresses, phones e-mails of all municipal dependencies
+        </p>
+        <span className="office-hours">
+          You can call or write from 08 am to 14 pm
+        </span>
+      </div>
       <div className="grid-container" id="contacts-grid">
         {contacts.map((contact) => {
           return (
