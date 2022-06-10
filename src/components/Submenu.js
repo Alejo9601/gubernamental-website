f7bd59975ae96714a1efd381a619d9092231a7f1
Alejo9601 <alejandro96jm@gmail.com>
2022-06-09 23:00:11 -0300

    Modified submenu text value

--- src/components/Submenu.js
+++ src/components/Submenu.js
@@ -4,10 +4,11 @@ const Submenu = ({ submenuItems }) => {
   return (
     <ol className="submenu">
       {submenuItems.map((item, index) => {
+        const textValue = item.politicianId
         return (
           <li key={item.politicianId.concat(index)}>
             <Link to={`${item.baseUrl}/${item.politicianId}`}>
-              {item.politicianId}
+              {textValue.charAt(0).toUpperCase() + textValue.slice(1)}
             </Link>
           </li>
         );
