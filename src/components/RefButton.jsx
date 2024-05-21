8f33caf78b3b12b45c39bbd153754b64d9f6a491
Alejo9601 <alejandro96jm@gmail.com>
2024-05-21 18:27:50 -0300

    Modified styles for button

--- src/components/RefButton.jsx
+++ src/components/RefButton.jsx
@@ -1,18 +1,16 @@
 const RefButton = ({ reference, btnText }) => {
-  return (
-    <div className="flex-container ref-button">
-      <div id="background-color-container">
-        <a
-          style={{ textDecoration: "none" }}
-          href={reference}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          {btnText}
-        </a>
+   return (
+      <div className="flex-container ref-button">
+         <a
+            style={{ textDecoration: "none" }}
+            href={reference}
+            target="_blank"
+            rel="noreferrer noopener"
+         >
+            {btnText}
+         </a>
       </div>
-    </div>
-  );
+   );
 };
 
 export default RefButton;
