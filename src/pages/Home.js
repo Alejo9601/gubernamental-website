2214745719cee4a93d3f08066ea280d30b9b1e25
Alejo9601 <alejandro96jm@gmail.com>
2024-05-21 16:17:15 -0300

    Modified name component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,17 @@
-import Carousel from "../components/Carousel";
+import ImagesCarousel from "../components/ImagesCarousel";
 import NewsSlider from "../components/NewsSlider";
 import Procedures from "../components/Procedures";
 import Contact from "../components/Contact";
 
 const Home = () => {
-  return (
-    <>
-      <Carousel />
-      <NewsSlider />
-      <Procedures />
-      <Contact />
-    </>
-  );
+   return (
+      <>
+         <ImagesCarousel />
+         <NewsSlider />
+         <Procedures />
+         <Contact />
+      </>
+   );
 };
 
 export default Home;
