b85a7d49f6d3a347b16427f027d9d436a6c0c169
Alejo9601 <alejandro96jm@gmail.com>
2024-05-27 17:53:43 -0300

    Implemented component FormSubmitButton

--- src/components/Contact/ContactForm/index.jsx
+++ src/components/Contact/ContactForm/index.jsx
@@ -1,38 +1,40 @@
-const ConsultForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+import FormSubmitButton from "../../FormSubmitButton";
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        className="personal-data"
-        type="text"
-        placeholder="Introduce your full name ..."
-        name="name"
-      />
-      <input
-        className="personal-data"
-        type="text"
-        placeholder="What is the subject?"
-        name="last-name"
-      />
-      <input
-        className="personal-data"
-        type="email"
-        placeholder="Introduce your e-mail ..."
-        name="email"
-      />
-      <textarea
-        className="query"
-        rows="10"
-        cols="20"
-        wrap="soft"
-        value="Write your message ..."
-      />
-      <input className="send-data" type="submit" value="Submit" />
-    </form>
-  );
+const ConsultForm = () => {
+   const handleSubmit = (e) => {
+      e.preventDefault();
+   };
+
+   return (
+      <form onSubmit={handleSubmit}>
+         <input
+            className="personal-data"
+            type="text"
+            placeholder="Introduce your full name ..."
+            name="name"
+         />
+         <input
+            className="personal-data"
+            type="text"
+            placeholder="What is the subject?"
+            name="last-name"
+         />
+         <input
+            className="personal-data"
+            type="email"
+            placeholder="Introduce your e-mail ..."
+            name="email"
+         />
+         <textarea
+            className="query"
+            rows="10"
+            cols="20"
+            wrap="soft"
+            value="Write your message ..."
+         />
+         <FormSubmitButton textValue="Enviar" />
+      </form>
+   );
 };
 
 export default ConsultForm;
