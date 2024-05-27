b725b008a04cc9666a98cbb2efc21dd2d0b557d2
Alejo9601 <alejandro96jm@gmail.com>
2024-05-27 15:38:23 -0300

    Created component Contact

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,12 @@
+import ContactForm from "./ContactForm";
+import "../../styles/contact.css";
+
+const Contact = () => {
+   return (
+      <section className="section-content" id="contact">
+         <ContactForm />
+      </section>
+   );
+};
+
+export default Contact;
