5187210e37772f66531cf9b93b6cd43e4e350c74
Alejo9601 <alejandro96jm@gmail.com>
2024-05-27 17:11:04 -0300

    Added wrapper

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,16 @@ import ImagesCarousel from "../components/ImagesCarousel";
 import NewsSlider from "../components/NewsSlider";
 import ClientServices from "../components/ClientServices";
 import Contact from "../components/Contact";
+import "../../src/styles/home.css";
 
 const Home = () => {
    return (
       <>
          <ImagesCarousel />
-         <NewsSlider />
-         <ClientServices />
+         <main className="main-wrapper">
+            <NewsSlider />
+            <ClientServices />
+         </main>
          <Contact />
       </>
    );
