a0f0e217ab3d0d15d609bab87cb4d34d4df695e6
Alejo9601 <alejandro96jm@gmail.com>
2024-05-28 18:07:32 -0300

    Implemented Component TourismPromotion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import NewsSlider from "../components/NewsSlider";
 import ClientServices from "../components/ClientServices";
 import Contact from "../components/Contact";
 import "../../src/styles/home.css";
+import TourismPromotion from "../components/TourismPromotion";
 
 const Home = () => {
    return (
@@ -10,6 +11,7 @@ const Home = () => {
          <ImagesCarousel />
          <main className="main-wrapper">
             <NewsSlider />
+            <TourismPromotion />
             <ClientServices />
          </main>
          <Contact />
