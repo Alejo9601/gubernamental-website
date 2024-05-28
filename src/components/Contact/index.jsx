0d6bcba58ea7b3052496238b3a003e3ad34c2aa6
Alejo9601 <alejandro96jm@gmail.com>
2024-05-28 16:03:53 -0300

    Implemented component and added styles

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,17 @@
 import ContactForm from "./ContactForm";
 import "../../styles/contact.css";
+import MapLocation from "../MapLocation";
 
 const Contact = () => {
    return (
       <section className="section-content" id="contact">
-         <ContactForm />
+         <div className="content-wrapper">
+            <h2>Ponte en contacto con nosotros</h2>
+            <div className="contact-map-wrapper">
+               <ContactForm />
+               <MapLocation />
+            </div>
+         </div>
       </section>
    );
 };
