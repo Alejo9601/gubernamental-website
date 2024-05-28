eec32cbb1ac13e2d81ede6351b4bb316259b9d24
Alejo9601 <alejandro96jm@gmail.com>
2024-05-28 18:07:09 -0300

    Created component

---
+++ src/components/TourismPromotion/index.jsx
@@ -0,0 +1,13 @@
+import Button from "../Button";
+import "../../styles/tourismPromotion.css";
+
+export default function TourismPromotion() {
+   return (
+      <section className="section-content" id="tourism-promotion">
+         <div className="banner">
+            <h2>Conocé nuestra localidad</h2>
+            <Button btnText="Ver mas" />
+         </div>
+      </section>
+   );
+}
